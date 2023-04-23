8e27251d1467fa4ad8f26ca77762a01e08ec5c4c
HOANGNBD <Duchoang020120@gmail.com>
2023-04-23 09:29:20 +0700

    update

--- Reactjs/src/hooks/useAuthStore.tsx
+++ Reactjs/src/hooks/useAuthStore.tsx
@@ -39,3 +39,14 @@ export const useAuthStore = create(
     )
   )
 );
+
+//persist ( DUY TRÌ TRẠNG THÁI CỦA STORE'STATE qua các lần TẢI LẠI, STATE KHÔNG THAY ĐỔI KHI F5)
+// Zustand's persist is another optional package that can be used
+// to persist your store's state across page reloads or browser sessions.
+
+// With persist, you can store your store's state in localStorage, sessionStorage,
+// or any other custom storage solution. This way, when the user refreshes or closes the page,
+// the state is automatically restored when the page is reopened.
+
+// Devtools
+// Usage with a plain action store, it will log actions as "setState"
--- Reactjs/src/pages/HomePage.tsx
+++ Reactjs/src/pages/HomePage.tsx
@@ -1,5 +1,105 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Column } from "@ant-design/plots";
+import { Bar } from "@ant-design/plots";
 
-export default function HomePage() {
-  return <div>HomePage</div>;
-}
+const HomePage = () => {
+  const [data1, setData1] = useState([]);
+  const [data2, setData2] = useState([]);
+  const [data3, setData3] = useState([]);
+
+  useEffect(() => {
+    asyncFetch1();
+    asyncFetch2();
+    asyncFetch3();
+  }, []);
+
+  const asyncFetch1 = () => {
+    fetch("http://localhost:9000/questions/24")
+      .then((response) => response.json())
+      .then((json) => setData1(json))
+      .catch((error) => {
+        console.log("fetch data failed", error);
+      });
+  };
+
+  const newData1 = data1.map((item: any) => ({
+    ...item,
+    fullName: `${item.firstName}${item.lastName}`,
+  }));
+
+  const config1 = {
+    data: newData1,
+    xField: "fullName",
+    yField: "total_sales",
+    label: {
+      style: {
+        fill: "#FFFFFF",
+        opacity: 0.6,
+      },
+    },
+    xAxis: {
+      label: {
+        autoHide: true,
+        autoRotate: false,
+      },
+    },
+    meta: {
+      fullName: {
+        alias: "姓名",
+      },
+      sales: {
+        alias: "销售额",
+      },
+    },
+  };
+
+  const asyncFetch2 = () => {
+    fetch("http://localhost:9000/questions/18")
+      .then((response) => response.json())
+      .then((json) => setData2(json))
+      .catch((error) => {
+        console.log("fetch data failed", error);
+      });
+  };
+  const config2 = {
+    data: data2,
+    xField: "numberOfProducts",
+    yField: "name",
+    seriesField: "name",
+    // legend: {
+    //   position: "left",
+    // },
+  };
+
+  const asyncFetch3 = () => {
+    fetch("http://localhost:9000/questions/19")
+      .then((response) => response.json())
+      .then((json) => setData3(json))
+      .catch((error) => {
+        console.log("fetch data failed", error);
+      });
+  };
+  const config3 = {
+    data: data3,
+    xField: "numberOfProducts",
+    yField: "name",
+    seriesField: "name",
+    // legend: {
+    //   position: "left",
+    // },
+  };
+  return (
+    <>
+      <h1>Employee's total sales</h1>
+      <Column {...config1} />
+
+      <h1>Number of goods in each category</h1>
+      <Bar {...config2} />
+
+      <h1>Number of goods in each supplier</h1>
+      <Bar {...config3} />
+    </>
+  );
+};
+
+export default HomePage;
