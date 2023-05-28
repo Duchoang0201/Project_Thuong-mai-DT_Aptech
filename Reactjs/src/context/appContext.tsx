34c1e2e22b26df96481d61e397d02ef23a6371c9
JasonP2711 <cthang2711@gmail.com>
2023-05-28 08:57:22 +0700

    delete

--- Reactjs/src/context/appContext.tsx
+++
@@ -1,6 +0,0 @@
-import { io } from "socket.io-client";
-import React from "react";
-const SOCKET_URL = process.env.REACT_APP_BASE_URL || "http://localhost:9000";
-export const socket = io(SOCKET_URL);
-// app context
-export const AppContext = React.createContext("");
