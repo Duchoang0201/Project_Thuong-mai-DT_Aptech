b6eca2d980bbc223b8bddf43eae078bf20ed8577
JasonP2711 <cthang2711@gmail.com>
2023-06-06 10:12:36 +0700

    delete

--- Nodejs/constants/dbSettings.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  // CONNECTION_STRING: 'mongodb+srv://developer:147258369@cluster0.w8qpr.mongodb.net/test',
-  // DATABASE_NAME: 'test',
-  CONNECTION_STRING:
-    "mongodb+srv://duchoang:duchoangproject@project.ihnuzpi.mongodb.net/test",
-  DATABASE_NAME: "test",
-};
